125ccce33743d5cb3c0f518901c2311e2232e66b
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-03-31 01:32:03 +0600

    Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -5,23 +5,43 @@ const player = {
   name: "Sakib Al hasan",
   age: 45
 }
+const singers = [
+  {name: 'Ashraful Islam', id: 403}, 
+  {name: 'Mardia Akter', id: 285},
+  {name: 'Islam', id: 3}, 
+  {name: 'Akter', id: 85}
+];
 const musicStyle = {
   color : "gray",
   backgroundColor : "black",
   
 }
 function App() {
+  const vegetables = ['alo', 'fulkopi', 'badha kopi','potol','begun', 'misti kumra', 'tomato'];
+  const friendNames = ['Ashraful', 'Mardia', 'Tauhidul', 'Samin', 'Farhan', 'Uccash']
   return (
     <div className="App">
-      <Person name = "ashraful" surName = "islam"></Person>
-      <Person name="rubel" naika="xyz"></Person>
-      <Friend></Friend>
+      {/* {
+        vegetables.map(vegetable => <li>Vegetable: {vegetable}</li>)
+      } */}
+      {
+        vegetables.map(vegetable => <Person name={vegetable}></Person>)
+      }
+      {
+        friendNames.map(friendName => <Friend name={friendName}></Friend>)
+      }
+      {
+        singers.map(singer => <Person name={singer.name} id={singer.id}></Person>)
+      }
+      {/* <Person name = "ashraful" surName = "islam"></Person>
+      <Person name="rubel" naika="xyz"></Person> */}
+      {/* <Friend></Friend>
       <Person></Person>
       <Friend></Friend>
       <Person></Person>
       <Friend></Friend>
       <Person></Person>
-      <Friend></Friend>
+      <Friend></Friend> */}
     </div>
   );
 }
@@ -30,15 +50,15 @@ function Person(props) {
   return (
   <div className='person'>
     <h1>{props.name} {props.surName} {props.naika}</h1>
-    <p>Profession: Cricket</p>
+    <p>{props.id}</p>
   </div>
   )
 }
 
-function Friend() {
+function Friend(props) {
   return (
     <div className='friend'>
-      <h3>Name: Ajay</h3>
+      <h3>Name: {props.name}</h3>
       <p>Job: maramari</p>
     </div>
   )
