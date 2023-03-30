8c34ad3ba4f72aa0cdabdcf816e35062f44ce10f
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-03-30 22:42:00 +0600

    2nd Video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+const num = 45;
+const player = {
+  name: "Sakib Al hasan",
+  age: 45
+}
+const musicStyle = {
+  color : "gray",
+  backgroundColor : "black",
+  
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>hello JSX</h1>
+         <div className="container">
+          <h3>Hello dude react , How are you</h3>
+         </div>
+         <div className="music">
+          <p style={musicStyle} >Name: {55+ num}</p>
+          <p style={musicStyle} >Player name: {player.name}</p>
+          <p style={{color : "gray", backgroundColor : "pink"}}>Age: {player.age}</p>
+         </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
